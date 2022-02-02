8371786bf8f4e61327d6efaae4890d47c1d891b5
shubhangi1208 <ashubhangi1996@gmail.com>
2022-02-02 19:51:17 +0530

    ADDED:Exception Handling When User Provide Invalid Mood

--- src/com.bridgelabz/java/com/bridgelabz/MoodAnalyzer.java
+++ src/com.bridgelabz/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,11 +1,34 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public String analyseMood(String message) {
-        if(message.contains("SAD"))
-            return "SAD";
-        else
-        return "HAPPY";
+    public String message;
+
+    //DEFAULT CONSTRUCTOR
+    public MoodAnalyzer() {
+
+    }
+
+    //PARAMETERISED CONSTRUCTOR
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    //METHOD TO ANALYZE MOOD
+    public String analyseMood() {
+        {
+            try {
+                if (message.contains("Sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+            return "HAPPY";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("******************** WELCOME TO MOOD ANALYSER ********************");
     }
 }
-
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -2,18 +2,28 @@ import com.bridgelabz.MoodAnalyzer;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MoodAnalyzerTest extends MoodAnalyzer {
+public class MoodAnalyzerTest{
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String result = moodAnalyzer.analyseMood("User is SAD");
-        Assert.assertEquals("SAD", result);
-    }
-    @Test
-    public void givenMessage_WhenHappy_ShouldReturnSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String result = moodAnalyzer.analyseMood("User is HAPPY");
-        Assert.assertEquals("HAPPY", result);
+    public void givenMessage_WhenMatchSad_ThenReturnSad()
+    {
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in Sad Mood");
+        String mood=moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD", mood);
     }
 
+    @Test
+    public void givenMessage_WhenMatchHappy_ThenReturnHappy()
+    {
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer("I am in Happy Mood");
+        String mood=moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenMatchNull_ThenReturnHappy()
+    {
+        MoodAnalyzer moodAnalyser=new MoodAnalyzer(null);
+        String mood=moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
